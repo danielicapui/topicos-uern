d9d776afff1c607ec422a85ca4d6e35b2cab1cde
danielicapui <37754709+danielicapui@users.noreply.github.com>
2021-08-15 18:10:49 -0300

    Atualizando arquivos e adicionando documentação

---
+++ utills/README.txt
@@ -0,0 +1,5 @@
+Execute o makefile para gerar a biblioteca utills.so
+para compilar o arquivo principal use make:main
+Para limpar o projeto digite make:limpa
+você pode adiciona o cabeçalho utills.h em seus arquivos cpp
+e compilar usando o comando g++ filename.cpp -o filename
--- utills/utills.h
+++ utills/utills.h
@@ -2,49 +2,168 @@
 #define UTILLS_H
 #include <vector>
 #include <iostream>
-//g++ -c encadeada.cpp
-//ar crv encadeada.a encadeada.o
-//g++ main.cpp -o programa encadeada.a
-//Cria biblioteca
-//g++ -c -fpic encadeada.cpp
-//g++ -shared -o encadeada.so encadeada.o
-//teste
-//g++ -L. -Wl,-rpath=. -Wall -o programa main.cpp -lencadeada
-
-//Já tinha feito implementação em c, c++ e em python dela mas essa é baseado no modelo do professor. Só que utilizando dois struct
-//Esse struct usamos para os nós da lista
-    typedef struct no
-    {
-        int dado;
-        no *prox;
-    }no;
-    //A lista propriamente dita, veja que tem dois ponteiros que guardam a posição do começo e fim da lista
-    //Como também o tamanho da lista
-    typedef struct Lista{
-        no *start, *end;
-        int tam;
-    }Lista;
-    void createLista(Lista *);
-    int handle_exception();
-    void addStart(Lista *,int);
-    void addEnd(Lista *,int);
-    void showLista(Lista *);
-    void tamLista(Lista *);
-    void removeLista(Lista *,int);
-    bool inLista(Lista *,int);
-    no* findElement(Lista *,int);
-    std::vector<int> toVector(Lista *);
-    void toLista(std::vector<int>&,Lista *);
-    void mostrarVetor(std::vector<int>&);
-    int particao(std::vector<int>&,int,int);
-    //vetor,0,tamanho-1
-    void quick_sort(std::vector<int>&,int,int);
-    void trocar(std::vector<int>&,int,int);
-    int particiona(std::vector<int>&,int,int,int);
-    void quick_sortImperativo(std::vector<int>&,int,int);
-    void deleteLista(Lista *);
-    void orderLista(Lista *);
-    int buscaBinariaRecursiva(int,int,int*,int);
-    int buscaBinariaImperativa(int,int*,int);
+/**
+ * define um tipo de struct no que armazena inteiros e o proximo no
+ *
+ * Example:
+ * no* aux=lista->start;
+ * aux->dado;
+ * aux->prox;
+ */
+typedef struct no
+{
+    int dado;
+    no *prox;
+} no;
+/**
+ * define um tipo Lista que guarda o começo e o fim como também o tamanho dela dinamicamente
+ *
+ * Example:
+ * Lista lista;
+ * lista->tam=0;
+ * lista->start=NULL;
+ * lista->end=NULL;
+ */
+typedef struct Lista
+{
+    no *start, *end;
+    int tam;
+} Lista;
+/**
+ * iniciar uma lista, primeira operação
+ * Example:
+ * createLista(&lista);
+ */
+void createLista(Lista *);
+//uma função que gera inteiros seguros
+int handle_exception();
+/**
+ * inserção no começo
+ *
+ * Example:
+ * addStart(&lista,5);
+ */
+void addStart(Lista *, int);
+/**
+ * inserção no fim
+ *
+ * Example:
+ * addEnd(&lista,5);
+ */
+void addEnd(Lista *, int);
+/**
+ * exibe o conteudo de uma lista
+ * Example:
+ * showLista(&lista);
+ */
+void showLista(Lista *);
+/**
+ * exibe o tamanho da lista
+ *
+ * Example:
+ * tamlista(&lista);
+ */
+void tamLista(Lista *);
+/**
+ * remove um elemento
+ *
+ * Example:
+ * removeLista(&lista,5);
+ */
+void removeLista(Lista *, int);
+/**
+ * verifica se um elemento está na lista e retorna true ou false
+ *
+ * Example:
+ * inLista(&lista,2);
+ */
+bool inLista(Lista *, int);
+/**
+ * verifica se um elemento está na lista e retorna o no
+ *
+ * Example:
+ * findElement(&lista,2);
+ */
+no *findElement(Lista *, int);
+/**
+ * transforma uma lista em vetor de mesmo tamanho
+ *
+ * Example:
+ * toVector(&lista);
+ */
+std::vector<int> toVector(Lista *);
+/**
+ * transforma em lista a partir de um std::vector<int>
+ *
+ * Example:
+ * toLista(vetor,&lista);
+ */
+void toLista(std::vector<int> &, Lista *);
+/**
+ * exibe um vetor std::vector<int>
+ *
+ * Example:
+ * mostrarVetor(vetor);
+ */
+void mostrarVetor(std::vector<int> &);
+/**
+ * particiona um vetor função do quick_sort
+ *
+ * Example:
+ * particao(vetor,inicio,fim);
+ */
+int particao(std::vector<int> &, int, int);
+/**
+ * ordenar um elemento std::vector<int> um array primitivo tambem é possivel com uma pequena alteração;
+ *
+ * Example:
+ * quick_sort(vetor,0,vetor.size()-1);
+ */
+void quick_sort(std::vector<int> &, int, int);
+/**
+ * troca a posição de dois números em um vetor
+ * Example:
+ * trocar(vetor,4,5);
+ */
+void trocar(std::vector<int> &, int, int);
+/**
+ * particiona um vetor função do quick_sort
+ *
+ * Example:
+ * particao(vetor,inicio,fim);
+ */
+int particiona(std::vector<int> &, int, int);
+/**
+ * ordenar um elemento std::vector<int> um array primitivo tambem é possivel com uma pequena alteração;
+ * de modo imperativo ou interado
+ * Example:
+ * quick_sort(vetor,0,vetor.size()-1);
+ */
+void quick_sortImperativo(std::vector<int> &, int, int);
+/**
+ * excluir os elementos de uma lista e deixa vazia
+ * Example:
+ * deleteLista(&lista);
+ */
+void deleteLista(Lista *);
+/**
+ * ordenar uma lista utilizando a classe de std::vector como controle, todos os elementos serão excluidos e será
+ * chamado a função de ordenação quick_sort o resultado será transformado em uma lista e adicionado na lista original.
+ * Example:
+ * orderLista(&lista);
+ */
+void orderLista(Lista *);
+/**
+ * busca um número em um vetor e retorna sua posição de forma recursiva
+ * Example:
+ * buscaBinariaRecursiva(primeiro,ultimo,vetor,numero_desejado);
+ */
+int buscaBinariaRecursiva(int, int, int *, int);
+/**
+ * busca um número em um vetor e retorna sua posição de forma imperativa
+ * Example:
+ * buscaBinariaImperativa(tamanho_vetor,vetor,numero_desejado);
+ */
+int buscaBinariaImperativa(int, int *, int);
 
 #endif
